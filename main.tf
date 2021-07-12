7454d9b00c8e31f234f7e5e076b49590193bdb2b
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2021-07-12 15:51:55 +0100

    INITIAL COMMIT

---
+++ main.tf
@@ -0,0 +1,178 @@
+terraform {
+  required_providers {
+    azurerm = {
+      version = "~>2.67.0"
+      source  = "hashicorp/azurerm"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  environment = var.environment
+}
+
+// azure-security-benchmark-foundation
+data "azurerm_client_config" "current" {}
+
+data "azurerm_subscription" "current" {}
+
+locals {
+  subscription_id = length(var.subscription_id) > 0 ? var.subscription_id : data.azurerm_subscription.current.id
+  nw_location     = length(var.nw_location) > 0 ? var.nw_location : "[parameters('hubLocation')]"
+}
+
+resource "azurerm_resource_group" "base_blueprint" {
+  name     = "${var.prefix}-base-blueprint"
+  location = var.location
+}
+
+resource "azurerm_role_definition" "azbf" {
+  role_definition_id = "5269e321-b006-72a2-d7ef-3122e1f9f7a0"
+  name               = "azbf-blueprint-role"
+  scope              = local.subscription_id
+
+  permissions {
+    actions     = ["Microsoft.Authorization/locks/write"]
+    not_actions = []
+  }
+
+  assignable_scopes = [
+    local.subscription_id,
+  ]
+}
+
+data "azurerm_blueprint_definition" "azbf" {
+  name     = "azure-security-benchmark-foundation"
+  scope_id = local.subscription_id
+}
+
+data "azurerm_blueprint_published_version" "azbf" {
+  scope_id       = data.azurerm_blueprint_definition.azbf.scope_id
+  blueprint_name = data.azurerm_blueprint_definition.azbf.name
+  version        = "1.0"
+}
+
+resource "azurerm_user_assigned_identity" "id" {
+  resource_group_name = azurerm_resource_group.base_blueprint.name
+  location            = var.location
+  name                = "${var.prefix}-user-identity"
+}
+
+resource "azurerm_role_assignment" "operator" {
+  scope                = local.subscription_id
+  role_definition_name = "Blueprint Operator"
+  principal_id         = azurerm_user_assigned_identity.id.principal_id
+}
+
+resource "azurerm_role_assignment" "azbf" {
+  scope              = local.subscription_id
+  role_definition_id = azurerm_role_definition.azbf.role_definition_resource_id
+  principal_id       = azurerm_user_assigned_identity.id.principal_id
+}
+
+resource "azurerm_role_assignment" "contributor" {
+  scope                = local.subscription_id
+  role_definition_name = "Contributor"
+  principal_id         = azurerm_user_assigned_identity.id.principal_id
+}
+
+resource "azurerm_blueprint_assignment" "azbf" {
+  name                   = "${var.prefix}azbf-assigment"
+  target_subscription_id = local.subscription_id
+  version_id             = data.azurerm_blueprint_published_version.azbf.id
+  location               = var.location
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.id.id]
+  }
+
+  parameter_values = <<VALUES
+    {
+        "namePrefix": {
+            "value": "${var.prefix}"
+        },
+        "hubName": {
+            "value": "env-hub"
+        },
+        "deployHub": {
+            "value": true
+        },
+        "hub-shared-network-firewall_azureFirewallPrivateIP": {
+            "value": "10.0.0.4"
+        },
+        "destinationAddresses": {
+            "value": "0.0.0.0"
+        },
+        "hub-shared-network-nsg_enableNsgFlowLogs": {
+            "value": true
+        },
+        "hub-shared-network-vnet_vnetAddressPrefix": {
+            "value": "10.0.0.0/16"
+        },
+        "hub-shared-network-vnet_azureFirewallSubnetAddressPrefix": {
+            "value": "10.0.0.0/26"
+        },
+        "hub-shared-network-vnet_bastionSubnetAddressPrefix": {
+            "value": "10.0.1.0/27"
+        },
+        "hub-shared-network-vnet_gatewaySubnetAddressPrefix": {
+            "value": "10.0.2.0/24"
+        },
+        "hub-shared-network-vnet_managementSubnetAddressPrefix": {
+            "value": "10.0.3.0/24"
+        },
+        "hub-shared-network-vnet_jumpBoxSubnetAddressPrefix": {
+            "value": "10.0.4.0/24"
+        },
+        "hub-shared-network-vnet_optionalSubnetNames": {
+            "value": []
+        },
+        "hub-shared-network-vnet_optionalSubnetPrefixes": {
+            "value": []
+        },
+        "enableDdosProtection": {
+            "value": false
+        },
+        "networkWatcherResourceGroupLocation": {
+            "value": "${local.nw_location}"
+        },
+        "spokeName": {
+            "value": "spoke-rke2"
+        },
+        "spoke-workload-network-vnet_spokeVnetAddressPrefix": {
+            "value": "10.1.0.0/16"
+        },
+        "spoke-workload-network-vnet_spokeSubnetAddressPrefix": {
+            "value": "10.1.0.0/24"
+        },
+        "spoke-workload-network-vnet_spokeOptionalSubnetNames": {
+            "value": [
+                "cluster"
+            ]
+        },
+        "spoke-workload-network-vnet_spokeOptionalSubnetPrefixes": {
+            "value": [
+                "10.1.16.0/20"
+            ]
+        },
+        "deploySpoke": {
+            "value": true
+        },
+        "hubLocation": {
+            "value" : "${var.location}"
+        }
+    }
+  VALUES
+
+  depends_on = [
+    azurerm_role_assignment.operator,
+    azurerm_role_assignment.contributor,
+    azurerm_role_assignment.azbf
+  ]
+  timeouts {
+    create = "180m"
+    delete = "180m"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "prefix" {
+  type    = string
+  default = "dpos"
+}
+
+variable "environment" {
+  type    = string
+  default = "usgovernment"
+}
+
+variable "location" {
+  type    = string
+  default = "usgovarizona"
+}
+
+variable "subscription_id" {
+  type    = string
+  default = ""
+}
+
+variable "nw_location" {
+  type    = string
+  default = ""
+}
